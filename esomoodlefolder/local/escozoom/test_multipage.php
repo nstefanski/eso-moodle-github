f3cfbdcbb93b598a1be1975d981c394fd0485ede
nstefanski <nstefanski@escoffier.edu>
2016-05-20 14:58:18 -0500

    Escoffier Zoom
    Packaging some of our custom code that make use of the Zoom API into a
    local plugin so we can update the Zoom activity mod independently

---
+++ esomoodlefolder/local/escozoom/test_multipage.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('../../config.php');
+
+global $DB, $CFG;
+
+require_once($CFG->dirroot.'/mod/zoom/classes/webservice.php');
+require_once($CFG->dirroot.'/local/escozoom/escoffierlib.php');
+
+echo "Hello world!<br />";
+
+//$url = 'user/get';
+//$data = array('id' => 'O4sZU-3NTQyMjteKgAr71g');
+
+$service = new mod_zoom_webservice();
+//$response = $service->make_call('user/getbyemail', array('email' => 'nstefanski@escoffier.edu', 'login_type' => '100'));
+
+$response = make_call_multipage('report/getaccountreport', array('from' => '2016-1-24', 'to' => '2016-2-5'));
+
+print_r($response);
+
+echo "<br />Bonjour le monde!";
