d6072463258a49bb0c67a67ed642a8b4450ca3ba
nstefanski <nstefanski@escoffier.edu>
2016-07-26 17:10:18 -0500

    class for updating attendance and more tests

--- esomoodlefolder/local/campusvue/tests/test.php
+++ esomoodlefolder/local/campusvue/tests/test.php
@@ -24,19 +24,69 @@ require_once('../../../config.php');
 global $CFG;
 require_once($CFG->dirroot.'/local/campusvue/lib.php');
 require_once($CFG->dirroot.'/local/campusvue/classes/cvEntityMsg.php');
+//require_once($CFG->dirroot.'/local/campusvue/classes/cvAttendanceMsg.php');
+require_once($CFG->dirroot.'/local/campusvue/classes/cvAttendancesMsg.php');
 
-echo cvGetToken() . '<hr>';
+echo 'cvGetToken: ' . substr(cvGetToken(), 0, 20) . ' ...<hr>';
 
-$cem = new cvEntityMsg('Student');
-$fieldName = 'StudentNumber';
-$fieldValue = 1607222765;
+$entityType = 'ClassAttendance';
+echo "GetSearchableAttributes for $entityType : ";
+$cem = new cvEntityMsg($entityType);
+print_R($cem->getSearchableAttributes() );
+echo '<hr>';
+
+$cem = new cvEntityMsg($entityType);
+$fieldName = 'ClassSchedId';
+$fieldValue = 3653;
 $paramOperator = 'Equal';
 $cem->addParam($fieldName, $fieldValue, $paramOperator);
-print_R($cem);
-//$cem->makeOrClause();
-//$cem->addParam($fieldName, $fieldValue, $paramOperator);
-echo '<br><br>' . $cem->getEntity()->Id;
+/*$fieldName = 'Date';
+$fieldValue = '2016-07-13T00:00:00';
+$paramOperator = 'Equal';
+$cem->addParam($fieldName, $fieldValue, $paramOperator);*/
+print_R($cem->getEntity() );
+//echo 'cvEntityMsg: ' . $cem->getEntityField('LengthMinutes') . '<hr>';
 
-echo '<br><br>' . $cem->getEntityField('Id');
-//$clause = $cem->EntitySearchClauses[0]->Operator;
-//echo $clause;
+/*$studentid = 206552;
+$csid = 3653;
+$attdate = '2016-07-13T00:00:00';
+$minsAbsent = 13;
+echo 'cvAttendanceMsg (string): ';
+$cam = new cvAttendanceMsg($studentid, $csid, $attdate, $minsAbsent);
+print_R($cam);
+
+$attdate = 1469527200;
+echo '<br><br>cvAttendanceMsg (unix timestamp): ';
+$cam = new cvAttendanceMsg($studentid, $csid, $attdate, $minsAbsent);
+print_R($cam);
+echo '<br><br>zeroTime(): ';
+$cam->zeroTime();
+print_R($cam);
+echo '<br><br>incrementDay(): ';
+$cam->incrementDay();
+print_R($cam);
+
+$attdate = 1470632700000;
+echo '<br><br>cvAttendanceMsg (unix ms timestamp): ';
+$cam = new cvAttendanceMsg($studentid, $csid, $attdate, $minsAbsent);
+print_R($cam);
+
+$attdate = new DateTime('2016-07-13 00:00:00');
+echo '<br><br>cvAttendanceMsg (php datetime): ';
+$cam = new cvAttendanceMsg($studentid, $csid, $attdate, $minsAbsent);
+print_R($cam);
+echo '<hr>';*/
+
+$cam = new cvAttendancesMsg();
+$sid = 206552;
+$csid = 3653;
+$attdates = array('2016-07-12T00:00:00',1468386000,'2016-07-14T00:00:00',new DateTime('2016-07-15 00:00:00'));
+$minsAbsent = array(12,13,14,15);
+foreach ($attdates as $key => $attdate) {
+	$cam->addAttendanceZeroTime($sid, $csid, $attdate, $minsAbsent[$key], 0, true);
+}
+echo 'cvAttendancesMsg: ';
+print_R($cam);
+echo '<br><br>';
+//print_R($cam->postAttendanceTransaction(null,null,true));
+echo '<hr>';
