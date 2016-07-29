a33ae9abd476cc1e64838344f152abf8e7761c04
nstefanski <nstefanski@escoffier.edu>
2016-07-29 17:12:12 -0500

    window.setTimeout to fix premature run

--- esomoodlefolder/local/campusvue/js/cvConvertTextInputsToNumeric.js
+++ esomoodlefolder/local/campusvue/js/cvConvertTextInputsToNumeric.js
@@ -1,10 +1,14 @@
 function cvConvertTextInputsToNumeric() {
-	var i,inputs = document.getElementById('region-main').getElementsByTagName('input');
-	for (i = 0; i < inputs.length; i++) {
-		if (inputs[i].type == 'text') {
-			inputs[i].type = 'number';
-			inputs[i].min = '0';
-			inputs[i].removeAttribute('maxlength');
+	if (document.getElementById("block-region-side-pre")==null) {
+		window.setTimeout(cvConvertTextInputsToNumeric, 100);
+	} else { 
+		var i,inputs = document.getElementById('region-main').getElementsByTagName('input');
+		for (i = 0; i < inputs.length; i++) {
+			if (inputs[i].type == 'text') {
+				inputs[i].type = 'number';
+				inputs[i].min = '0';
+				inputs[i].removeAttribute('maxlength');
+			}
 		}
 	}
 }
