2aa74d8bd3d14072b6eb4ded57ff586211a0d0a7
nstefanski <nstefanski@escoffier.edu>
2016-08-03 13:40:10 -0500

    now script also checks date and disables input fields for days past

---
+++ esomoodlefolder/local/campusvue/js/cvConvertInputs.js
@@ -0,0 +1,26 @@
+function cvConvertInputs() {
+	if (document.getElementById("block-region-side-pre")==null) {
+		window.setTimeout(cvConvertInputs, 100);
+	} else { 
+		var past = false;
+		var dateStr = document.getElementById('region-main').getElementsByTagName('td')[0].innerHTML;
+		dateStr = dateStr.substring(0, dateStr.search('<'));
+		if (isNaN(Date.parse(dateStr)) == false) {
+			var d = new Date(dateStr);
+			var today = new Date();
+			today.setHours(0,0,0,0);
+			past = d < today;
+		}
+		var i,inputs = document.getElementById('region-main').getElementsByTagName('input');
+		for (i = 0; i < inputs.length; i++) {
+			if (inputs[i].type == 'text') {
+				inputs[i].type = 'number';
+				inputs[i].min = '0';
+				inputs[i].removeAttribute('maxlength');
+			}
+			if (past) {
+				inputs[i].setAttribute("disabled",1);
+			}
+		}
+	}
+}
