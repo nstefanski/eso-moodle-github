a8761c08a04d161d7f7d3a04bcf24bd5ae2879cd
nstefanski <nstefanski@escoffier.edu>
2016-08-08 10:20:01 -0500

    block_contactstudent
    custom block for recording contact

---
+++ esomoodlefolder/blocks/contact_student/block_contact_student.php
@@ -0,0 +1,134 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The contact_student block
+ *
+ * @package    block_contact_student
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+// Obviously required
+require_once('contact_student_form.php');
+
+class block_contact_student extends block_base {
+
+    function init() {
+        $this->title = get_string('pluginname', 'block_contact_student');
+    }
+
+    /**
+     * Set the applicable formats for this block to all
+     * @return array
+     */
+    function applicable_formats() {
+        if (has_capability('moodle/site:config', context_system::instance())) {
+            return array('all' => true);
+        } else {
+            return array('site' => true);
+        }
+    }
+
+    function instance_allow_multiple() {
+        return false;
+    }
+
+    function get_content() {
+        global $CFG, $PAGE, $DB, $USER;
+        if ($this->content !== NULL) {
+            return $this->content;
+        }
+        $context = context_system::instance();
+        if (!has_capability('block/contact_student:viewinstance', $context)) {
+            return $this->content;
+        }
+
+        $userid = optional_param('id', 0, PARAM_INT);
+
+        //get user details
+        if (!empty($userid)) {
+            $user = $DB->get_record('user', array('id' => $userid));
+        }
+
+        if (isset($user) && isset($USER)) {
+            require_once($CFG->dirroot . '/user/profile/lib.php');
+            profile_load_custom_fields($user);
+            if (array_key_exists('cvueid', $user->profile)) {
+                $cvueid = $user->profile['cvueid'];
+            }
+            $firstname = $user->firstname;
+            $lastname = $user->lastname;
+            $email = $user->email;
+            if (array_key_exists('startdate', $user->profile)) {
+                $timecreated = date('Y-m-d', $user->profile['startdate']);
+            }
+            $instructorname = fullname($USER);
+            $instructoremail = $USER->email;
+            $studentfullname = fullname($user);
+        }
+        $contact_student_form = new contact_student_form();
+
+        if ($data = $contact_student_form->get_data()) {
+
+            $record = new stdClass();
+            $record->student_id = $userid;
+            $record->cvueid = $cvueid;
+            $record->instructor_id = $USER->id;
+            $record->contact_type = $data->contacttype;
+            $record->call_reason = $data->callreason;
+            $record->notes = $data->notes;
+            $record->time_created = time();
+            $recordid = $DB->insert_record('block_contact_student', $record);
+
+            $flag_records = array();
+            for ($i = 1; $i < 8; $i++) {
+                $option = 'redflagsoption' . $i;
+                if (isset($data->$option)) {
+                    $flag_record = new stdClass();
+                    $flag_record->contact_id = $recordid;
+                    $flag_record->red_flag = get_string($option, 'block_contact_student');
+                    $flag_records[] = $flag_record;
+                }
+            }
+            $DB->insert_records('block_contact_student_flags', $flag_records);
+
+            $userprofile_url = new moodle_url('/user/profile.php', array('id' => $userid));
+            redirect($userprofile_url);
+        } else {
+            $contact_student_form->set_data(
+                    array(
+                        'id' => $userid,
+                        'studentcvueid' => $cvueid,
+                        'studentfirstname' => $firstname,
+                        'studentlastname' => $lastname,
+                        'studentemail' => $email,
+                        'studentstartdate' => $timecreated,
+                        'instructorname' => $instructorname,
+                        'instructoremail' => $instructoremail,
+                        'displaystudentname' => $studentfullname
+            ));
+            $form_html = $contact_student_form->render();
+
+            $this->content = new stdClass();
+            $this->content->text = $form_html;
+            $this->content->footer = '';
+            return $this->content;
+        }
+    }
+
+}
---
+++ esomoodlefolder/blocks/contact_student/contact_student_form.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once("{$CFG->libdir}/formslib.php");
+
+class contact_student_form extends moodleform {
+
+    function definition() {
+
+        $mform = $this->_form;
+        $mform->addElement('text', 'id', get_string('studentid', 'block_contact_student'));
+        $mform->hardFreeze('id');
+        
+        $mform->addElement('text', 'studentcvueid', get_string('studentcvueid', 'block_contact_student'));
+        $mform->hardFreeze('studentcvueid');
+
+        $mform->addElement('text', 'studentfirstname', get_string('studentfirstname', 'block_contact_student'));
+        $mform->hardFreeze('studentfirstname');
+
+        $mform->addElement('text', 'studentlastname', get_string('studentlastname', 'block_contact_student'));
+        $mform->hardFreeze('studentlastname');
+
+        $mform->addElement('text', 'studentemail', get_string('studentemail', 'block_contact_student'));
+        $mform->hardFreeze('studentemail');
+
+        $mform->addElement('text', 'studentstartdate', get_string('studentstartdate', 'block_contact_student'));
+        $mform->hardFreeze('studentstartdate');
+
+        $mform->addElement('text', 'instructorname', get_string('instructorname', 'block_contact_student'));
+        $mform->hardFreeze('instructorname');
+
+        $mform->addElement('text', 'instructoremail', get_string('instructoremail', 'block_contact_student'));
+        $mform->hardFreeze('instructoremail');
+
+        $mform->addElement('text', 'displaystudentname', get_string('displaystudentname', 'block_contact_student'));
+        $mform->hardFreeze('displaystudentname');
+
+        $contactoptions = array('moodlemessage' => '',
+            'phone' => get_string('phone', 'block_contact_student'),
+            'text' => get_string('text', 'block_contact_student'),
+            'email' => get_string('email', 'block_contact_student'));
+
+        $mform->addElement('select', 'contacttype', get_string('contacttype', 'block_contact_student'), $contactoptions);
+        $mform->addHelpButton('contacttype', 'contacttype', 'block_contact_student');
+        
+        $callreasonoptions = array('' => '',
+            get_string('24hourcall', 'block_contact_student') => get_string('24hourcall', 'block_contact_student'),
+            get_string('studenttostudentcall', 'block_contact_student') => get_string('studenttostudentcall', 'block_contact_student'),
+            get_string('atriskcall', 'block_contact_student') => get_string('atriskcall', 'block_contact_student'),
+            get_string('other', 'block_contact_student') => get_string('other', 'block_contact_student'));
+
+        $mform->addElement('select', 'callreason', get_string('callreason', 'block_contact_student'), $callreasonoptions);
+
+        $mform->addElement('checkbox', 'redflagsoption1', get_string('redflags', 'block_contact_student'), get_string('redflagsoption1', 'block_contact_student', array('class' => 'redflag')));
+        $mform->addElement('checkbox', 'redflagsoption2', '', get_string('redflagsoption2', 'block_contact_student', array('class' => 'redflag')));
+        $mform->addElement('checkbox', 'redflagsoption3', '', get_string('redflagsoption3', 'block_contact_student', array('class' => 'redflag')));
+        $mform->addElement('checkbox', 'redflagsoption4', '', get_string('redflagsoption4', 'block_contact_student', array('class' => 'redflag')));
+        $mform->addElement('checkbox', 'redflagsoption5', '', get_string('redflagsoption5', 'block_contact_student', array('class' => 'redflag')));
+        $mform->addElement('checkbox', 'redflagsoption6', '', get_string('redflagsoption6', 'block_contact_student', array('class' => 'redflag')));
+        $mform->addElement('checkbox', 'redflagsoption7', '', get_string('redflagsoption7', 'block_contact_student', array('class' => 'redflag')));
+
+        $mform->addElement('textarea', 'notes', get_string('notes', 'block_contact_student'), 'wrap="virtual" rows="10" cols="25"');
+
+        $this->add_action_buttons(false, 'Submit');
+    }
+
+}
