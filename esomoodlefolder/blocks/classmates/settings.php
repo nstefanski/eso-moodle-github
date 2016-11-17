697d51eb0ca55fb9831482ae55ed0157ed19ffb6
nstefanski <nstefanski@escoffier.edu>
2016-11-17 11:59:42 -0600

    removing custom block

--- esomoodlefolder/blocks/classmates/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die;
-
-if ($ADMIN->fulltree) {
-    $settings->add(new admin_setting_configtext('block_classmates_timetosee', get_string('timetosee', 'block_classmates'),
-                   get_string('configtimetosee', 'block_classmates'), 30, PARAM_INT));
-}
-
